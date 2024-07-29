9244e14d48b8f26605328e492596532ada797120
Imelda Hope <imeldalarusso@gmail.com>
2024-07-30 02:40:25 +0300

    creates and runs tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,19 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  //Expect the DOM structure to be printed out on the terminal
+  screen.debug();
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
+
+//Or use test below to print out only the single element i.e. <div>please pass this test</div>
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
